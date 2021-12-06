07f81af128779e22a9805a9a8de4d7a227808ded
Chen Xi <75736258+devxichen@users.noreply.github.com>
2021-12-06 21:25:57 +0800

    [git] Add some config into InitGit.sh

--- Git/InitGit.sh
+++ Git/InitGit.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
 
+#Help:
+#Delete exist config:   git config --global --unset-all confignameA.confignameB
+#Set new config:        git config --global config_nameA.config_nameB config_value
+
+######### Safety #########
+
 #Clear essential git config
 git config --global --unset user.name
 git config --global --unset user.email
 
+#gi push need explicit argument
+git config --global push.default=nothing
+
+#Disable fast-forward as default
+git config --global merge.ff false
+
+######### Appearance and usage #########
+
+#Appearance
+git config --global color.ui true
+git config --global log.decorate true
+
 #Set default editor
 git config --global core.editor "vim"
+
+######### Shortcut #########
+
+#Graph log
+git config --global alias.glog "log --graph --pretty=oneline --abbrev-commit --color --decorate"
